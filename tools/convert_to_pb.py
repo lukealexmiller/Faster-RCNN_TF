c8daff3aae751fd8aad77784481564835d2f7f9b
Luke Miller <lukealexmiller@gmail.com>
2017-02-21 14:36:49 +0100

    Added parsing

--- tools/convert_to_pb.py
+++ tools/convert_to_pb.py
@@ -25,8 +25,28 @@ def parse_args():
     Parse input arguments
     """
     parser = argparse.ArgumentParser(description='Convert Checkpoint to ProtoBuf for Fast R-CNN network')
+    parser.add_argument('--device', dest='device', help='device to use',
+                        default='cpu', type=str)
+    parser.add_argument('--device_id', dest='device_id', help='device id to use',
+                        default=0, type=int)
+    parser.add_argument('--def', dest='prototxt',
+                        help='prototxt file defining the network',
+                        default=None, type=str)
     parser.add_argument('--weights', dest='model',
                         help='model to test',
+                        default=None, type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file', default=None, type=str)
+    parser.add_argument('--wait', dest='wait',
+                        help='wait until net file exists',
+                        default=False, type=bool)
+    parser.add_argument('--imdb', dest='imdb_name',
+                        help='dataset to test',
+                        default='voc_2007_test', type=str)
+    parser.add_argument('--comp', dest='comp_mode', help='competition mode',
+                        action='store_true')
+    parser.add_argument('--network', dest='network_name',
+                        help='name of the network',
                         default=None, type=str)
     parser.add_argument('--set', dest='set_cfgs',
                         help='set config keys', default=None,
