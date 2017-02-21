754e8d8ffd6211f896e9e8cca707811d365cd5b1
Luke Miller <lukealexmiller@gmail.com>
2017-02-21 17:02:05 +0100

    cleaned up a little

--- tools/convert_to_pb.py
+++ tools/convert_to_pb.py
@@ -60,30 +60,21 @@ def parse_args():
     return args
 
 def freeze_graph(network_name,model_path,model_file):
-    # We retrieve our checkpoint fullpath
-    #checkpoint = tf.train.get_checkpoint_state(model_path,model_file)
-    #input_checkpoint = checkpoint.model_checkpoint_path
-    input_checkpoint = model_path+model_file
-    print(input_checkpoint)
-    # We precise the file fullname of our freezed graph
-    #absolute_model_path = "/".join(input_checkpoint.split('/')[:-1])
+    # Define full path and filename of input checkpoint
+    input_checkpoint = model_path + model_file
+    # Define full path and filename of output protobuf file
     output_graph = model_path + os.path.splitext(model_file)[0] + ".pb"
-    print(output_graph)
+
     # Before exporting our graph, we need to specify our output nodes
     # This is how TF decides what part of the Graph he has to keep and what part it can dump
     # NOTE: this variable is plural, because you can have multiple output nodes
-    output_node_names = "cls_prob"
+    output_node_names = "bbox_pred"
 
     # We clear devices to allow TensorFlow to control on which device it will load operations
     clear_devices = True
 
     network = get_network(network_name)
     print 'Use network `{:s}` in training'.format(args.network_name)
-
-    #init = tf.global_variables_initializer()
-    # We import the meta graph and retrieve a Saver
-    
-    #saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=clear_devices)
 
     saver = tf.train.Saver()
     # We retrieve the protobuf graph definition
@@ -95,14 +86,14 @@ def freeze_graph(network_name,model_path,model_file):
         #sess.run(init)
         saver.restore(sess, input_checkpoint)
         print ('Loading model weights from {:s}').format(input_checkpoint)
-        #[n.name for n in tf.get_default_graph().as_graph_def().node]
-        print(output_node_names.split(","))
+
         # We use a built-in TF helper to export variables to constants
         output_graph_def = graph_util.convert_variables_to_constants(
             sess, # The session is used to retrieve the weights
             input_graph_def, # The graph_def is used to retrieve the nodes
             output_node_names.split(",") # The output node names are used to select the useful nodes
-        ) 
+        )
+        print('Saving output node names: {:s}').format(output_node_names.split(","))
 
         # Finally we serialize and dump the output graph to the filesystem
         with tf.gfile.GFile(output_graph, "wb") as f:
@@ -122,7 +113,7 @@ if __name__ == '__main__':
     pprint.pprint(cfg)
 
     while not os.path.exists(args.model_path) and args.wait:
-        print('Waiting for {} to exist...'.format(args.model_path))
+        print('Waiting for {} to exist...'.format(args.model_path+args.model_file))
         time.sleep(10)
 
     freeze_graph(args.network_name,args.model_path,args.model_file)
