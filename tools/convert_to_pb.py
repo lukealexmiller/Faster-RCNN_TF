e65a16b3e6805c78ec3d6c3f6288872eb8a41f30
Luke Miller <lukealexmiller@gmail.com>
2017-02-21 14:34:08 +0100

    Another approach

--- tools/convert_to_pb.py
+++ tools/convert_to_pb.py
@@ -18,34 +18,15 @@ import argparse
 import pprint
 import time, os, sys
 import tensorflow as tf
+from tensorflow.python.framework import graph_util
 
 def parse_args():
     """
     Parse input arguments
     """
-    parser = argparse.ArgumentParser(description='Test a Fast R-CNN network')
-    parser.add_argument('--device', dest='device', help='device to use',
-                        default='cpu', type=str)
-    parser.add_argument('--device_id', dest='device_id', help='device id to use',
-                        default=0, type=int)
-    parser.add_argument('--def', dest='prototxt',
-                        help='prototxt file defining the network',
-                        default=None, type=str)
+    parser = argparse.ArgumentParser(description='Convert Checkpoint to ProtoBuf for Fast R-CNN network')
     parser.add_argument('--weights', dest='model',
                         help='model to test',
-                        default=None, type=str)
-    parser.add_argument('--cfg', dest='cfg_file',
-                        help='optional config file', default=None, type=str)
-    parser.add_argument('--wait', dest='wait',
-                        help='wait until net file exists',
-                        default=False, type=bool)
-    parser.add_argument('--imdb', dest='imdb_name',
-                        help='dataset to test',
-                        default='voc_2007_test', type=str)
-    parser.add_argument('--comp', dest='comp_mode', help='competition mode',
-                        action='store_true')
-    parser.add_argument('--network', dest='network_name',
-                        help='name of the network',
                         default=None, type=str)
     parser.add_argument('--set', dest='set_cfgs',
                         help='set config keys', default=None,
@@ -57,6 +38,48 @@ def parse_args():
 
     args = parser.parse_args()
     return args
+
+def freeze_graph(model_folder):
+    # We retrieve our checkpoint fullpath
+    checkpoint = tf.train.get_checkpoint_state(model_folder)
+    input_checkpoint = checkpoint.model_checkpoint_path
+    
+    # We precise the file fullname of our freezed graph
+    absolute_model_folder = "/".join(input_checkpoint.split('/')[:-1])
+    output_graph = absolute_model_folder + "/frozen_model.pb"
+
+    # Before exporting our graph, we need to specify our output nodes
+    # This is how TF decides what part of the Graph he has to keep and what part it can dump
+    # NOTE: this variable is plural, because you can have multiple output nodes
+    output_node_names = "Accuracy/predictions"
+
+    # We clear devices to allow TensorFlow to control on which device it will load operations
+    clear_devices = True
+    
+    # We import the meta graph and retrieve a Saver
+    saver = tf.train.import_meta_graph(input_checkpoint + '.meta', clear_devices=clear_devices)
+
+    # We retrieve the protobuf graph definition
+    graph = tf.get_default_graph()
+    input_graph_def = graph.as_graph_def()
+
+    # We start a session and restore the graph weights
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+        saver.restore(sess, input_checkpoint)
+
+        print ('Loading model weights from {:s}').format(args.model)
+
+        # We use a built-in TF helper to export variables to constants
+        output_graph_def = graph_util.convert_variables_to_constants(
+            sess, # The session is used to retrieve the weights
+            input_graph_def, # The graph_def is used to retrieve the nodes
+            output_node_names.split(",") # The output node names are used to select the useful nodes
+        ) 
+
+        # Finally we serialize and dump the output graph to the filesystem
+        with tf.gfile.GFile(output_graph, "wb") as f:
+            f.write(output_graph_def.SerializeToString())
+        print("%d ops in the final graph." % len(output_graph_def.node))
 
 if __name__ == '__main__':
     args = parse_args()
@@ -74,33 +97,5 @@ if __name__ == '__main__':
         print('Waiting for {} to exist...'.format(args.model))
         time.sleep(10)
 
-    weights_filename = os.path.splitext(os.path.basename(args.model))[0]
-
-    imdb = get_imdb(args.imdb_name)
-    imdb.competition_mode(args.comp_mode)
-
-    device_name = '/{}:{:d}'.format(args.device,args.device_id)
-    print device_name
-
-    network = get_network(args.network_name)
-    print 'Use network `{:s}` in training'.format(args.network_name)
-
-    if args.device == 'gpu':
-        cfg.USE_GPU_NMS = True
-        cfg.GPU_ID = args.device_id
-    else:
-        cfg.USE_GPU_NMS = False
-
-    # start a session
-    saver = tf.train.Saver()
-    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
-    saver.restore(sess, args.model)
-    print ('Loading model weights from {:s}').format(args.model)
-
-    filename = os.path.splitext(args.model)[0] + '.pb'
-    output_dir = get_output_dir(imdb, None)
-
-    tf.train.write_graph(sess.graph_def, output_dir, "test.pb", False)
-    print 'Wrote ProtoBuf to: {:s}'.format(output_dir)
-
+    freeze_graph(args.model)
     
